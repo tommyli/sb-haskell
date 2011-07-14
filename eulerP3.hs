f5ca7a49812c0d7a7cd883e143550d5f2e97be6d
Tommy Yim Li <tommyli@me.com>
2011-07-15 01:38:10 +1000

    removed use of Set and pair

--- eulerP3.hs
+++ eulerP3.hs
@@ -1,38 +1,73 @@
 -- The prime factors of 13195 are 5, 7, 13 and 29.
 -- What is the largest prime factor of the number 600851475143 ?
 
-import Data.Set (Set)
-import qualified Data.Set as Set
+module Main (maxPrimeFactor) where
 
-factorsOf :: Integer -> Set Integer
-factorsOf 0 = Set.empty
-factorsOf 1 = Set.singleton 1
-factorsOf n =
-  let
-    sqrtList = [2..(round (sqrt $ fromInteger n+1))]
-    factorList = map (factorPair n) sqrtList
-  in
-    Set.fromList (foldr (\p l -> (fst p):(snd p):l) [] factorList)
+factorsOf :: Integer -> [Integer]
+factorsOf 0 = []
+factorsOf 1 = [1]
+factorsOf n = factorsOf' n (upToRoot n)
 
-factorPair :: Integer -> Integer -> (Integer, Integer)
-factorPair number testFactor =
-  if isFactor number testFactor == True then
-    (testFactor, number `div` testFactor)
+factorsOf' :: Integer -> [Integer] -> [Integer]
+factorsOf' x [] = [1, x]
+factorsOf' x (y:ys) =
+  if (isFactor x y) then
+    y:(x `div` y):(factorsOf' x ys)
   else
-    (0,0)
+    factorsOf' x ys
 
 isFactor :: Integer -> Integer -> Bool
-isFactor number testFactor =
-  if number `mod` testFactor == 0 then True else False
+isFactor number testFactor = number `mod` testFactor == 0
 
+upToRoot :: Integer -> [Integer]
+upToRoot n = [2..(round (sqrt $ fromInteger n+1))]
+-- upToRoot n = [x | x <- [2..n-1], x*x <= n]  -- has performance issues but don't know why
+
+isPrime :: Integer -> Bool
+isPrime 1 = False
+isPrime 2 = True
+isPrime n = length (factorsOf n) == 2
+
+maxPrimeFactor :: Integer -> Integer
+maxPrimeFactor n = maximum (filter isPrime (factorsOf n))
+
+-- main :: IO ()
+-- main = putStrLn $ show (maxPrimeFactor 600851475143)
+
+-- Very neat way of testing if a number is prime, found on Internet
+-- http://www.haskell.org/pipermail/haskell-cafe/2005-December/013130.html
+-- and performs very well too.
 prime 1 = False
 prime 2 = True
-prime n = filter (isFactor n) primes == [] where 
+prime n = filter (divides n) primes == [] where 
   numbers = [x | x <- [2..n-1], x*x <= n]
   primes = filter prime numbers
-
 divides a b = (mod a b == 0)
 
-pf :: Integer -> Integer
+-- Another neat way dealing with primes, from lecturer
+-- returns the (infinite) list of all primes
+-- The sieve of Eratosthenes
+all_primes :: [Integer]
+all_primes = prime_filter ([2..])
 
-pf n = Set.findMax (Set.filter prime (factorsOf n))
+prime_filter :: [Integer] -> [Integer]
+prime_filter [] = []
+prime_filter (x:xs) =
+  x:(prime_filter (filter_multiples x xs))
+  
+-- filter n xs: filter out multiples of n from xs
+filter_multiples :: Integer -> [Integer] -> [Integer]
+filter_multiples _ [] = []
+filter_multiples n (x:xs)
+  | (mod x n) == 0 =
+    filter_multiples n xs
+  | otherwise =
+    x:(filter_multiples n xs)
+
+primes_upto :: Integer -> [Integer]
+primes_upto n = primes_upto' n all_primes
+
+primes_upto' :: Integer -> [Integer] -> [Integer]
+primes_upto' n (p:ps)
+  | p > n = []
+  | otherwise = p:(primes_upto' n ps)
