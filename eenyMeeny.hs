5b49741b11e89b85a9a9123a18468a8119cbfb06
Tommy Yim Li <tommy.li@firefire.co>
2012-08-20 00:38:13 +1000

    Improved performance by using mod on k, tip from marsbomber

--- eenyMeeny.hs
+++ eenyMeeny.hs
@@ -1,11 +1,13 @@
 eenyMeeny :: Int -> Int -> ([Int], Int)
 eenyMeeny 1 _ = ([], 1)
-eenyMeeny n k = eenyMeeny' [0..(n-1)] k k []
+eenyMeeny n k = eenyMeeny' [0..(n-1)] 0 k []
 
 eenyMeeny' :: [Int] -> Int -> Int -> [Int] -> ([Int], Int)
 eenyMeeny' (n:[]) _ _ sequence = (sequence, n)
-eenyMeeny' (n:ns) currk k sequence =
-  if currk == 1 then
-    eenyMeeny' ns k k (sequence ++ [n])
-  else
-    eenyMeeny' (ns ++ [n]) (currk - 1) k sequence
+eenyMeeny' ns currPos k sequence =
+  eenyMeeny' (removeFromList ns indexToRemove) indexToRemove k $ sequence ++ [ns !! indexToRemove]
+  where indexToRemove = (currPos + k - 1) `mod` length ns
+
+removeFromList :: [Int] -> Int -> [Int]
+removeFromList [] _ = []
+removeFromList xs index = (take (index) xs) ++ (drop (index + 1) xs)
